8b5dfe6bc008ee92987999c240ae0d607a1e5cfa
Blake Israel <blake@honkforhelp.com>
2022-12-14 11:14:08 -0500

    Fix a bug when trying to run two honk-puma things at once on the same dev machine.
    We don't need the control server to run so don't do it.

--- bin/honk-puma-impl.sh
+++ bin/honk-puma-impl.sh
@@ -26,7 +26,12 @@ fi
 
 CONTROL_TOKEN="$RANDOM-$RANDOM-$RANDOM"
 
-# NOTE `watch` will take over your TTY in the console and make it where you can't really see the logs from Puma
-# This doesn't happen in heroku because it's not TTY.
-$1/bin/puma-status-service-agent.sh "$PUMA_STATE_FILE" &
-bundle exec puma --state "$PUMA_STATE_FILE" --control-url "tcp://127.0.0.1:$CONTROL_PORT" --control-token "$CONTROL_TOKEN" -C "$1/config/honk-puma.rb"
+if [[ "$DYNO" = "" ]]; then
+  # Don't bother running the control server etc. if we're not on Heroku,
+  # if you run more than one project with this Gem locally you'll get into a problem
+  # where the second honk-puma can't bind to the control port.
+	bundle exec puma -C "$1/config/honk-puma.rb"
+else
+  $1/bin/puma-status-service-agent.sh "$PUMA_STATE_FILE" &
+  bundle exec puma --state "$PUMA_STATE_FILE" --control-url "tcp://127.0.0.1:$CONTROL_PORT" --control-token "$CONTROL_TOKEN" -C "$1/config/honk-puma.rb"
+fi
